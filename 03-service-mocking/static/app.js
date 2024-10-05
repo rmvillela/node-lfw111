c3bdb517c422fd67d09f924bde72df7e032a61fe
Roosevelt Villela <rmvillela@outlook.com>
2024-10-05 09:58:09 -0300

    feat: Remoção do data do arquivo javascript client-side e adaptação do código para chamar um serviço externo que será criado na próxima etapa.

--- 03-service-mocking/static/app.js
+++ 03-service-mocking/static/app.js
@@ -1,29 +1,13 @@
-const mockData = [
-  {
-    id: "A1",
-    name: "Vacuum Cleaner",
-    rrp: "99.99",
-    info: "The most powerful vacuum in the world.",
-  },
-  {
-    id: "A2",
-    name: "Leaf Blower",
-    rrp: "303.33",
-    info: "This product will blow your socks off.",
-  },
-  {
-    id: "B1",
-    name: "Chocolate Bar",
-    rrp: "22.40",
-    info: "Deliciously overpriced chocolate.",
-  },
-];
+const API = "http://localhost:3000";
 
-const populateProducts = () => {
+const populateProducts = async () => {
   const products = document.querySelector("#products");
   products.innerHTML = "";
 
-  for (const product of mockData) {
+  const res = await fetch(API);
+  const data = await res.json();
+
+  for (const product of data) {
     const item = document.createElement("product-item");
 
     for (const key of ["name", "rrp", "info"]) {
