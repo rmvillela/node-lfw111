d9a03643c03a9ae7330ab59b1d40731d772c4ece
Roosevelt Villela <rmvillela@outlook.com>
2024-10-05 11:07:43 -0300

    feat: entramos no módulo de mocking get routes, onde iniciamos as mudanças nos arquivos client-side para efetivamente utilizar métodos http nas requests

--- 03-service-mocking/static/app.js
+++ 03-service-mocking/static/app.js
@@ -1,28 +1,27 @@
 const API = "http://localhost:3000";
 
-const populateProducts = async () => {
+const populateProducts = async (category) => {
   const products = document.querySelector("#products");
   products.innerHTML = "";
-
-  const res = await fetch(API);
+  const res = await fetch(`${API}/${category}`);
   const data = await res.json();
 
   for (const product of data) {
     const item = document.createElement("product-item");
-
     for (const key of ["name", "rrp", "info"]) {
       const span = document.createElement("span");
       span.slot = key;
       span.textContent = product[key];
       item.appendChild(span);
     }
-
     products.appendChild(item);
   }
 };
 
-document.querySelector("#fetch").addEventListener("click", async () => {
-  await populateProducts();
+const category = document.querySelector("#category");
+
+category.addEventListener("input", async ({ target }) => {
+  await populateProducts(target.value);
 });
 
 customElements.define(
@@ -30,8 +29,8 @@ customElements.define(
   class Item extends HTMLElement {
     constructor() {
       super();
-      const itemTmpl = document.querySelector("#item").content.cloneNode(true);
-      this.attachShadow({ mode: "open" }).appendChild(itemTmpl);
+      const itemTmpl = document.querySelector("#item").content;
+      this.attachShadow({ mode: "open" }).appendChild(itemTmpl.cloneNode(true));
     }
   }
 );
