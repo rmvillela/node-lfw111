e00de26edd85c18223e110c0bd4757c6301306ef
Roosevelt Villela <rmvillela@outlook.com>
2024-10-06 22:48:26 -0300

    feat: Criação do POST no backend
    Criação do método POST nas duas rotas de categorias no backend, onde utilizamos o decorator mockDataInsert para inserir os dados em memória de acordo com os dados da request e com os dados em memória atuais.

--- 03-service-mocking/mock-srv/routes/electronics/index.js
+++ 03-service-mocking/mock-srv/routes/electronics/index.js
@@ -15,8 +15,13 @@ const data = [
   },
 ];
 
-export default async function (fastify) {
+export default async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
+    return data;
+  });
+
+  fastify.post("/", async function (request, reply) {
+    fastify.mockDataInsert(request, opts.prefix.slice(1), data);
     return data;
   });
 }
