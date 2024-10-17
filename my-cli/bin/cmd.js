d04fd4877532b92cbfc79320986f376cc7651fd1
Roosevelt Villela <rmvillela@outlook.com>
2024-10-16 22:53:09 -0300

    feat: incremento da aplicação CLI com flags
    Instalação e implementação do pacote "commander" para passar a aceitar flags na aplicação CLI para fazer o update dos pedidos

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,45 +1,49 @@
 #!/usr/bin/env node
 
 import got from "got";
+import { Command } from "commander";
 
 const API = "http://localhost:3000";
 
 const usage = (msg = "Back office for My App") => {
   console.log(`\n${msg}\n`);
-  console.log("Usage: cmd <ID> <AMOUNT>");
 };
 
-/**
- * Recupera os argumentos da linha de comando retirando os dois primeiros itens do array que o process.argv retorna
- * O primeiro item contém o path completo para o binário atual do node.
- * O segundo item contém o path completo para o arquivo que está executando. Nesse caso do cmd.js.
- */
-const argv = process.argv.slice(2);
+async function updateItem(id, amount) {
+  usage(`Updating order ${id} with amount ${amount}`);
 
-/**
- *  se não tem argumentos, chama o método usage e finaliza
- */
-if (argv.length < 2) {
-  usage();
-  process.exit(1);
+  try {
+    if (isNaN(+amount)) {
+      usage("Error: <AMOUNT> must be a number");
+      process.exit(1);
+    }
+    // Use GOT to make a POST request to the API
+    await got.post(`${API}/orders/${id}`, {
+      json: { amount: +amount },
+    });
+    // Log the result to the console
+    usage(`Order ${id} updated with amount ${amount}`);
+  } catch (err) {
+    // If there is an error, log it to the console and exit
+    console.error(err.message);
+    process.exit(1);
+  }
 }
 
-const [argID, argAmount] = argv;
+const program = new Command();
 
-const amount = parseInt(argAmount);
+// Criação do programa
+program
+  .name("my-cli")
+  .description("Back office para o Programa de Estoque")
+  .version("1.0.0");
 
-if (isNaN(amount)) {
-  usage("Error: <AMOUNT> must be a number");
-  process.exit(1);
-}
+// Criação do comando de fazer o update de um pedido
+program
+  .command("update")
+  .argument("<ID>", "Order ID")
+  .argument("<AMOUNT>", "Order Amount")
+  .action(async (id, amount) => await updateItem(id, amount));
 
-try {
-  await got.post(`${API}/orders/${argID}`, {
-    json: { amount },
-  });
-
-  console.log(`Order ${argID} updated with amount ${amount}`);
-} catch (err) {
-  console.log(err.message);
-  process.exit(1);
-}
+// Parse dos argumentos de process.argv
+program.parse();
