6ddbb91de5e550964b93da54db0dd4ec337d928d
Roosevelt Villela <rmvillela@outlook.com>
2024-10-25 10:12:03 -0300

    feat: método de criação de produto na command line

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
-import { update } from "../src/utils";
+import { add, update } from "../src/utils.js";
 
 const program = new Command();
 
@@ -17,6 +17,21 @@ program
   .argument("<ID>", "Order ID")
   .argument("<AMOUNT>", "Order Amount")
   .action(async (id, amount) => await update(id, amount));
+
+// Criação do comando para implementar a nova funcionalidade de cadastro de produto
+program
+  .command("add")
+  .description("Create new product")
+  .argument("<CATEGORY>", "Product Category")
+  .argument("<ID>", "Product ID")
+  .argument("<NAME>", "Product Name")
+  .argument("<AMOUNT>", "Product RRP")
+  // INFO é opcional
+  .argument("[INFO...]", "Product Description")
+  .action(
+    async (category, id, name, amount, info) =>
+      await add(category, id, name, amount, info)
+  );
 
 // Parse dos argumentos de process.argv
 program.parse();
